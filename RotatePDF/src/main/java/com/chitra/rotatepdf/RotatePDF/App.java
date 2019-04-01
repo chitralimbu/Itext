ebad8e0ede8e32d48cbd3bb968d9b15f3a2adf55
chitralimbu <limbu_nikkil@hotmail.co.uk>
2019-04-01 23:24:00 +0100

    RotatePDF added

---
+++ RotatePDF/src/main/java/com/chitra/rotatepdf/RotatePDF/App.java
@@ -0,0 +1,13 @@
+package com.chitra.rotatepdf.RotatePDF;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
