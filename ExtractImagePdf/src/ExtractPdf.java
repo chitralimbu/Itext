d41eb16d25550ff7f5adda06c10d133aa1226b31
Chitra Limbu <limbu_nikkil@hotmail.co.uk>
2019-04-01 22:44:19 +0100

    Reforamatted

--- ExtractImagePdf/src/ExtractPdf.java
+++ ExtractImagePdf/src/ExtractPdf.java
@@ -11,46 +11,26 @@ import com.itextpdf.text.pdf.PdfStream;
 public class ExtractPdf {
 
 	public static void main(String[] args) throws IOException {
-		String path = "C:\\Users\\limbu\\Desktop\\v.pdf";
-		String path2 = "C:\\Users\\limbu\\Desktop\\pic";
-		
+		String path = "PATH";
+		String path2 = "PATH";
 		PdfReader reader = new PdfReader(path);
-		
 		for(int i = 0; i < reader.getXrefSize(); i++) {
-			
 			PdfObject pdfobj = reader.getPdfObject(i);
-			
 			if(pdfobj !=null) {
-				
 				if(pdfobj.isStream()) {
-					
 					PdfStream stream = (PdfStream) pdfobj;
-					
-					
 					PdfObject pdfsubtype = stream.get(PdfName.SUBTYPE);
-					
 					if(pdfsubtype!=null) {
-						
 						if(pdfsubtype.toString().equals(PdfName.IMAGE.toString())){
-							
 							byte[] img = PdfReader.getStreamBytesRaw((PRStream) stream);
-							
 							FileOutputStream out = new FileOutputStream(new File(path2 +  "\\image" + i + ".jpg"));
-							
 							out.write(img);
 							out.flush();
 							out.close();
-							
 						}
-						
 					}
-					
 				}
-				
 			}
-			
 		}
-
 	}
-
 }
